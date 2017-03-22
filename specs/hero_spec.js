016a6573302de54090a039650f7d944cba2c90ba
John Daly <jpdaly@gmail.com>
2017-03-22 16:10:58 +0000

    initial commit

---
+++ specs/hero_spec.js
@@ -0,0 +1,13 @@
+var Hero = require('../hero');
+var assert = require('assert');
+
+describe('Hero', function(){
+  it('has a name');
+
+  it('has health');
+
+  it('has a favourite food');
+
+  it('can say its name');
+
+})
